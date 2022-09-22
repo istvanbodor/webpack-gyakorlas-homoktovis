e47c0ede123bbef20e957645a99c0774831587a4
Bodor István <istvanbodor19@gmail.com>
2022-09-22 13:30:41 +0200

    haladas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import 'bootstrap/dist/js/bootstrap.bundle.js';
-import 'bootstrap/dist/css/bootstrap.css';
-import './main.css';
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/dist/css/bootstrap.css"
+import "./main.css";
+
