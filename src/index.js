f1073a514e8da5b3c071e2a3e4b517d0990f957d
Bodor István <istvanbodor19@gmail.com>
2022-09-25 15:11:35 +0200

    jskiserlet

--- src/index.js
+++ src/index.js
@@ -2,3 +2,19 @@ import "bootstrap/dist/js/bootstrap.bundle.js";
 import "bootstrap/dist/css/bootstrap.css"
 import "./main.css";
 
+function szinvaltas(e){
+    
+    document.getElementById('cimsor').style.color = e.currentTarget.value;
+}
+document.getElementById('szinvalto').addEventListener(change, szinvaltas);
+
+let i = 0;
+function kepklikk()
+{
+
+    document.getElementById('kepalairas').textContent = i++;
+
+}
+
+
+document.getElementById('bok').addEventListener('click', kepklikk)
